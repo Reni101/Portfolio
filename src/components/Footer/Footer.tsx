f31e60d95c706b738f9dcf0e70be2daae6aec666
Maxim Dmitriev <Renissance@tut.by>
2022-07-17 16:22:50 +0300

    Portfolio Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import style from './Footer.module.css'
 
 const Footer = () => {
     return (
-        <div>
+        <div className={style.mainFooterBlock}>
+            <div className={style.flexContainer}>
+                <h2>Maxim Dmitriev</h2>
+                <div className={style.socialContainer}>
+                    <div className={style.box}>VK</div>
+                    <div className={style.box}>F</div>
+                    <div className={style.box}>Twitter</div>
+                    <div className={style.box}>Youtube</div>
+                </div>
 
+            </div>
         </div>
     );
 };
