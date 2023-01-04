1002661db02c399a40c879aec990080d5d40ebba
maksimdmitriev <Renissance@tut.by>
2023-01-04 20:58:40 +0300

    cards

--- src/components/Projects/Project/Project.tsx
+++ src/components/Projects/Project/Project.tsx
@@ -3,23 +3,24 @@ import style from './Pкoject.module.css'
 
 type PropsType = {
     title: string
-    description:string
+    description: string
+    style: {backgroundImage:string}
 }
 
 
 const Project = (props: PropsType) => {
     return (
-        <div className={style.Project}>
-            <div className={style.ProjectIMG}>
+        <div className={style.Project} >
+            <div className={style.ProjectIMG} style={props.style}>
                 <div className={style.ProjectLinkDiv}>
-                    <a href={"https://reni101.github.io/it-incubator-todolist-ts/"} > details</a>
+                    <a href={"https://reni101.github.io/it-incubator-todolist-ts/"}> details</a>
                 </div>
 
             </div>
 
             <div className={style.ProjectDiscription}>
                 <h3 className={style.ProjectTittle}>{props.title}</h3>
-                <p >{props.description}</p>
+                <p>{props.description}</p>
             </div>
         </div>
     );
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,16 +1,34 @@
 import React from 'react';
 import style from './Projects.module.css'
 import Project from "./Project/Project";
+import social from '../../assets/social-network.webp'
+import todoList from '../../assets/to-do-list.jpeg'
+import cards from '../../assets/cards2.jpeg'
 
+
+const backgroundIMG = (urlIMG:string) => {
+    return { backgroundImage:`url(${urlIMG})`,}
+
+}
 const Projects = () => {
+
+
+
     return (
         <div className={style.MainProjectsContainer}>
             <div className={style.flexContainer}>
                 <h2>My projects</h2>
                 <div className={style.Projects}>
-                    <Project title={"Social-Network"} description={"bla bla bla bla bal"}/>
-                    <Project title={"TodoList"} description={"bla bla bla bla bal"}/>
-                    <Project title={"Cards"} description={"bla bla bla bla bal"}/>
+                    <Project title={"Social-Network"}
+                             description={"bla bla bla bla bal"}
+                             style={backgroundIMG(social)}
+                    />
+                    <Project title={"TodoList"}
+                             style={backgroundIMG(todoList)}
+                             description={"bla bla bla bla bal"}/>
+                    <Project title={"Cards"}
+                             style={backgroundIMG(cards)}
+                             description={"bla bla bla bla bal"}/>
 
                 </div>
             </div>
